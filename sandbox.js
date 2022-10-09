3216cfdebe50db33b8dae6746873cce66ad6136d
Ayesha jabeen <aj5643410@gmail.com>
2022-10-10 01:53:08 +0530

    js

---
+++ sandbox.js
@@ -0,0 +1,25 @@
+//strings
+console.log('hello,world');
+
+let email='aj5643410@gmail.com';
+console.log(email);
+
+
+//string concatenation
+let firstname='ayesha';
+let lastname='jabeen';
+let fullname=firstname+ '  ' +lastname;
+console.log(fullname);
+
+
+//getting characters
+console.log(fullname[0]);
+
+
+//string length
+console.log(fullname.length);
+
+//string methods
+console.log(fullname.toUpperCase());
+let result=fullname.toLocaleLowerCase();
+console.log(result);
