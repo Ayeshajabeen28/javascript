51f57edf4563c715b4b70e7fce409987a2dc374a
Ayesha jabeen <114102646+Ayeshajabeen28@users.noreply.github.com>
2022-10-14 01:20:02 +0530

    Add files via upload

--- sandbox.js
+++ sandbox.js
@@ -1,25 +1,135 @@
 //strings
-console.log('hello,world');
+// console.log('hello,world');
 
-let email='aj5643410@gmail.com';
-console.log(email);
-
-
-//string concatenation
-let firstname='ayesha';
-let lastname='jabeen';
-let fullname=firstname+ '  ' +lastname;
-console.log(fullname);
+// let email='aj5643410@gmail.com';
+// console.log(email);
 
 
-//getting characters
-console.log(fullname[0]);
+// //string concatenation
+// let firstname='ayesha';
+// let lastname='jabeen';
+// let fullname=firstname+ '  ' +lastname;
+// console.log(fullname);
 
 
-//string length
-console.log(fullname.length);
+// //getting characters
+// console.log(fullname[0]);
 
-//string methods
-console.log(fullname.toUpperCase());
-let result=fullname.toLocaleLowerCase();
-console.log(result);
+
+// //string length
+// console.log(fullname.length);
+
+// //string methods
+// console.log(fullname.toUpperCase());
+// let result=fullname.toLocaleLowerCase();
+// console.log(result);
+
+//js2
+//common string methods
+
+//let email='ayeshashahraa@gmail.com';
+//let result=email.lastIndexOf('h');
+
+//let result=email.slice(0,3);
+
+//let result=email.substring(4,9);
+
+//let result=email.replace('y','n');
+
+
+
+
+//console.log(result);
+
+
+
+//let radius=10;
+//const pi=3.14
+
+//console.log(radius,pi);
+
+//math operators +,-,*,/,**,%
+
+//console.log(10/2);
+//let result=(radius%3);
+//let result=pi*radius**2;
+
+//order of operation-B I D M A S
+
+//let result=5*(10-3)**2;
+
+
+
+
+//console.log(result);
+
+
+
+
+
+
+//let likes = 10;
+//likes = likes + 2;
+//likes++
+//likes += 10;
+//likes -= 5;
+//likes *=2 ;
+//console.log(likes);
+
+
+
+
+//NaN - not a number
+
+//console.log(5/'hello');
+//console.log(5*'hello');
+
+//template strings
+//const title ='best reads of 2019';
+//const author =' mario';
+//const likes = 30;
+
+
+
+
+//concatenation way
+//let result ='the blog called'+tiitle +'by'+author+'has'+likes +'likes';
+//console.log(result);
+
+
+//tempalate string way
+//let result = 'the blog called ${title} by ${author} has ${likes} likes';
+
+
+//creating html templates
+
+//day3
+
+
+//for loops
+
+//for(let i = 0; i < 5;  i++);{
+    //console.log('in loop:' , i );
+
+//}
+//console.log('loop finished');
+
+//const names =[ 'ayesha','jabeen','nagma'];
+
+// for (let i =0; i< names.length; i++){
+//     //console.log(i);
+//     let html ='<div>${names[i]}</div>';
+//     console.log(html);
+
+// }
+
+//while loops
+const names =[ 'ayesha','jabeen','nagma'];
+//let i =0 ;
+
+//while(i<5);{
+   // console.log('in loop:', i);
+
+
+//}
+let i = 0;
