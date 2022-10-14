08736150faeada31b081c77049d12d944486867c
Ayesha jabeen <114102646+Ayeshajabeen28@users.noreply.github.com>
2022-10-15 00:35:55 +0530

    Add files via upload

---
+++ objects.js
@@ -0,0 +1,89 @@
+//object literals
+// let user = {
+//     name : 'crystal',
+//     age : 30,
+//     email : 'crysral@mail.com',
+//     location :'berlin',
+//     blogs:['why mac & cheese rules',' 10 things to make with marmite']
+// };
+// console.log(user);
+// console.log(user,name);
+// //user.age = 35;
+// console.log(user.age);
+// //const key = 'location';
+// console.log(user['location']);
+// user['name']= 'chun-li';
+// console.log(user['name']);
+
+// console.log(typeof user);
+
+//object literals
+//const blogs =[
+    // {title:'why maac & cheese rules',likes:30},
+    // {title: '10 things to make with marmite', likes:50}
+//];
+//console.log(blogs);
+
+
+// let user = {
+//     name : 'crystal',
+//     age : 30,
+//     email : 'crysral@mail.com',
+//     location :'berlin',
+//     blogs:[
+//         {title:'why maac & cheese rules',likes:30},
+//     {title: '10 things to make with marmite', likes:50}
+//     ],
+//     login(){
+//      console.log('the user logged in');
+//     },
+//     logout(){
+//         console.log('the user logged out');
+
+//     },
+//     logblogs: function () {
+//         // //console.log(this);
+//         console.log('this user ha written the following blogs:');
+//         this.blogs.forEach(blogs => {
+//             console.log(blogs);
+
+
+//         });
+//         console.log(this);
+        
+//     }
+// };
+// user.login();
+// user.logout();
+// user.logblogs();
+// console.log(this);
+// const name ='ayesha';
+// name.toUpperCase();
+
+
+//math objects
+
+
+// console.log(Math);
+// console.log(Math.E);
+// console.log(MATH.PI);
+// //Console.log(Math.PI);
+// Console.log(Math.E);
+// const area =7.7;
+// Console.log(Math.round(area));
+// Console.log(Math.floor(area));
+// Console.log(Math.ceil(area));
+// Console.log(Math.trunc(area));
+// //random numbers
+
+// const random = Math.random();
+
+// console.log(random);
+// console.log(Math.round(random*100));
+
+//primitive values
+// let scoreone =50;
+// let scoreTwo = scoreone;
+// console.log('scoreone: ${scoreone}','scoretwo:${scoretwo}');
+// scoreone =100;
+// console.log('scoreone: ${scoreone}','scoretwo:${scoretwo}');
