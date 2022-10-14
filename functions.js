518262ea0423e940226abe4982ba990172e38b07
Ayesha jabeen <114102646+Ayeshajabeen28@users.noreply.github.com>
2022-10-14 18:23:06 +0530

    Add files via upload

---
+++ functions.js
@@ -0,0 +1,71 @@
+//function declaaration
+//function greet(){
+//console.log('hello there');
+
+
+//}
+
+// greet();
+// greet();
+//speak();
+//function declaaration
+
+//arguments & paramters
+
+// const speak = function(name){
+//     console.log('good day $(name)');
+// };
+
+// speak('ayesxhsas');
+
+// const speak = function(name,time){
+//     console.log('good day ${time} ${name}');
+// };
+
+// speak();
+// const speak = function(name = 'ayesha',time = 'night'){
+//     console.log('good  ${time} ${name}');
+// };
+
+// speak();
+// speak( 'jabeen' , 'day');
+
+//returining values
+//const speak = function(name = 'lunfi', time ='night'){
+//console.log('good ${time}${name}'):
+//};
+
+
+//arrow function 
+// const calcArea = radiud => 3.14* radius**2;
+
+// const area = calcArea(5);
+// console.log('area is:',area);
+
+
+//oractice arrow functions
+
+// const greet = function(){
+//     return 'hello, world';
+
+// };
+// const greet = ()=> 'hello , world';
+//  const result = greet();
+// console.log(result);
+
+// const bil = functuon(products,tax){
+//     let total = 0 ;
+//     for( let i=0; i < products.length; i++){
+
+
+//     }
+// }
+// const name = 'ayesha';
+
+// //functions
+
+// const greet = () => 'hello';
+// let resultone = greet();
+// console.log(resultOne);
+// //methods
+// let resultTwo = name.toUpperCase();
