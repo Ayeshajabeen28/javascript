0033db13e6454ee0276860033982d4a3a9034238
Ayesha jabeen <114102646+Ayeshajabeen28@users.noreply.github.com>
2022-10-15 17:08:46 +0530

    Add files via upload

--- objects.js
+++ objects.js
@@ -87,3 +87,73 @@
 // console.log('scoreone: ${scoreone}','scoretwo:${scoretwo}');
 // scoreone =100;
 // console.log('scoreone: ${scoreone}','scoretwo:${scoretwo}');
+ 
+
+// const para = document.querySelector('p');
+
+// console.log(para);
+// const para = document.querySelector('.error');
+
+// console.log(para);
+// const para = document.querySelector('div.error');
+
+// console.log(para);
+
+// const para = document.querySelector('body > h1');
+
+// console.log(para);
+//const para = document.querySelectorAll('p');
+//para.forEach(para => 
+//console.log(para)
+//const document.querySelector('.error');
+
+//console.log(para[0]);
+//console.log(error);
+
+// get an element by ID
+
+// const title = document.getElementbyId('page-title');
+// console.log(title);
+
+// //get elements by their class name
+// const errors = document.getElementsByClassName('error');
+// console.log(errors);
+// console.log(errors[0]);
+
+//get elements by their tag name
+
+
+
+// const paras = document.getElementsByTagName('p');
+// console.log(paras);
+// console.log(paras[0]);
+
+//const para = document.querySelector('p');
+
+//console.log(para.innerText);
+//para.innerText = 'ninjas are awesome!';
+
+// const paras = document.querySelectorAll('p');
+// paras.forEach(para =>{
+
+//     console.log(para.innertext);
+//     para.innerText +='new text';
+// });
+
+// const link = document.querySelector('a');
+
+// console.log(link.getAttribute('href'));
+
+// link.setAttribute('href', 'https://www.theayesha.com');
+// link.innertext = 'theayesha website';
+
+// const mssg = document.querySelector('p');
+
+
+// console.log(mssg.getAttribute('class'));
+// mssg.setAttribute('class','success');
+// mssg.setAttribute('style','color:red');
+
+//const title = document.querySelector('h1');
+
+//title.setAttribute('style','margin50px;');
