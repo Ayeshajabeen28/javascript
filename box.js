0ba9b8043cbed9c539ce218ab02186c45dee29f7
Ayesha jabeen <114102646+Ayeshajabeen28@users.noreply.github.com>
2022-10-18 23:00:54 +0530

    Add files via upload

---
+++ box.js
@@ -0,0 +1,10 @@
+const copy = document.querySelector('.copy-me');
+copy.addEventListener('copy',() => {
+    console.log('OI! my content is copyright' );
+
+});
+const box = document.querySelector('.box');
+box.addEventListener('mousemove', e=>{
+    //console.log(e.offsetX,e.offsetY);
+
+});
---
+++ document.js
@@ -0,0 +1,48 @@
+// const content = document.querySelector('p');
+
+
+// console.log(content.classList);
+// content.classList.add('error');
+// content.classList.remove('error');
+
+
+//const paras = document.querySelectorAll('p');
+
+
+//paras.forEach(p =>{
+
+    //console.log(p.textContent);
+//})
+
+//onst article = document.querySelector('article');
+// console.log(article.children);
+// console.log(Array.from(article.children));
+// console.log(article.children);
+
+// Array.from(article.children).forEach((child => {
+//     child.classList.add('article-element');
+
+
+
+// });
+
+// const title = document.querySelector('h2');
+
+// console.log(title.parentElement);
+// console.log(title.parentElement.parentElement);
+
+
+// const button = document.querySelector('button');
+
+// button.addEventListener('click',()=>{
+//     console.log('you clicked me');
+// });
+
+// const items = document.querySelectorAll('li');
+// items.forEach(item =>
+//     {
+//         item.addEventListener('click', ()=>{
+//             console.log('items');
+
+//         })
+//     })
