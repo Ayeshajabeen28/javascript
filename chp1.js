56692d0eaf1f3bb267deec6fed6194cea80b18ea
Ayesha jabeen <114102646+Ayeshajabeen28@users.noreply.github.com>
2022-10-23 19:34:50 +0530

    Add files via upload

---
+++ chp1.js
@@ -0,0 +1,60 @@
+// //dates & times
+
+// const now = new Date();
+
+// console.log(now);
+// console.log(typeof now);
+
+
+
+// //year,months,day,times
+
+// console.log('getfullyear:',now.getFullYear());
+// console.log('getmonth:',now.getMonth());
+// console.log('getDate:',now.getDate());
+// console.log('getDay:',now.getDay());
+// console.log('getHours:',now.getHours());
+// console.log('getMinutes:',now.getMinutes());
+// console.log('getSeconds:',now.getSeconds());
+
+// //time stamps
+// console.log('timestamp:',now.getTime);
+
+// //date strings
+
+// console.log(now.toDateString);
+
+
+//timestamps
+
+
+
+// const now = new Date();
+
+// console.log(now.getTime());
+
+
+
+
+// const clock =  document.querySelector('.clock');
+
+// const tick = () => {
+//     const now = new Date();
+
+//     const h = now.getHours();
+//     const m = now.getMinutes();
+//     const s = now.getSeconds();
+
+//     const html =  `
+//     <span>${h}</span>    
+//     <span>${m}</span>
+//     <span>${s}</span>
+    
+// `;
+// clock.innerHTML=html;
+
+//     console.log(h);
+
+// };
+
+// setInterval(tick,1000);
