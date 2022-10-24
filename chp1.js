55304b32f2a402499541ab0fe9d47df72eaca7ce
Ayesha jabeen <114102646+Ayeshajabeen28@users.noreply.github.com>
2022-10-24 16:45:55 +0530

    Add files via upload

--- chp1.js
+++ chp1.js
@@ -55,6 +55,15 @@
 
 //     console.log(h);
 
-// };
+// };c
 
 // setInterval(tick,1000);
+
+
+// const now = new Date();
+
+// //console.log(dateFns.isToday(now));
+
+// //formatting options
+
+// console.log(dateFns.format(now,'YYYY'));
